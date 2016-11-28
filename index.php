3625b2aa7f53f006f6cb291fa4a47d0e662d764a
xyy1024 <745283750@qq.com>
2016-11-28 16:27:22 +0800

    add a file to wenhuitianxia

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	//定义项目名称
+	define('APP_NAME', 'App');
+	//定义项目路径
+	define('APP_PATH', './');
+	define('RUNTIME_PATH','./Runtime/');
+	define('DB_BACKUP','./Sql/');
+	define('APP_DEBUG', TRUE);
+	require './_Core/ThinkPHP.php';
+?>
